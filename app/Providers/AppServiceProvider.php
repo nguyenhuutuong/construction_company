ec1d11fde609bcadc50ab79e958f0f971bc012a0
Nguyễn Hữu Tường <nguyenhuutuong.na@gmail.com>
2025-11-28 08:58:34 +0700

    add images

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 use App\Models\Service;
 use App\Models\HomeType;
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        }   
         Paginator::useBootstrap();
         // Tạo biến global
         View::share('MenuServices', Service::select('name', 'slug')
